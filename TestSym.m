211538f706318dd4b234bc945e9fe9d10c6dbeba
Denis Rosset <physics@denisrosset.com>
2017-02-02 03:29:55 +0800

    Removed verbose output in tests.

--- TestSym.m
+++ TestSym.m
@@ -6,11 +6,9 @@ rho1 = rho1/trace(rho1);
 v = sdpvar;
 rhov = rho0*(1-v) + rho1*v;
 coeffsv = CoeffsFromOperator2(rhov, 2, 2);
-disp('Without symmetry basis')
 Cons = SymmetricExtensionCone(coeffsv, 4, false, true);
 optimize(Cons, -v, options);
 vnosym = double(v);
-disp('With symmetry basis')
 Cons = SymmetricExtensionCone(coeffsv, 4, true, true);
 optimize(Cons, -v, options);
 vsym = double(v);
