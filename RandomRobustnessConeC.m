56f7b93d64d1bd4e1554f379da63e84f82f60b6a
Denis Rosset <physics@denisrosset.com>
2017-02-22 21:32:15 +0800

    Added comment for RandomRobustnessConeC.

--- RandomRobustnessConeC.m
+++ RandomRobustnessConeC.m
@@ -1,4 +1,13 @@
 function set = RandomRobustnessConeC(dims, k, ppt, useSym)
+% RandomRobustnessConeC Random robustness entanglement measure cone
+%
+% {nu rho} = RandomRobustnessConeC([dA dB]) returns the cone such that
+%
+% - rho is a bipartite density matrix, ordered such that th
+%   product state rhoAB = rhoA (x) rhoB = kron(rhoA, rhoB)
+% - nu is a nonnegative variable such that nu >= randomrobustness(rho)
+%   where randomrobustness(rho) is given by the Definition after Eq. (6)
+%   of http://link.aps.org/doi/10.1103/PhysRevA.59.141
     assert(length(dims) == 2);
     dA = dims(1);
     dB = dims(2);
