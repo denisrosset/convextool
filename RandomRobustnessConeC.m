cee2c6cf5808e68e63a861b29cc25a6aa4acf6d7
Denis Rosset <physics@denisrosset.com>
2017-02-22 23:49:23 +0800

    Added conditions of exactness when approximating the separable cone.

--- RandomRobustnessConeC.m
+++ RandomRobustnessConeC.m
@@ -1,5 +1,5 @@
 function set = RandomRobustnessConeC(def)
-% RandomRobustnessConeC Lower bound on the random robustness entanglement measure cone
+% RandomRobustnessConeC Outer approximation of the random robustness entanglement measure cone
 %
 % {nu rho} = RandomRobustnessConeC([dA dB]) returns the cone such that
 %
@@ -11,6 +11,9 @@ function set = RandomRobustnessConeC(def)
 %
 % The random robustness is computed with respect to an approximation of the
 % separable cone given in the parameter 'def', obtained by SymmetricExtensionDef.
+%
+% For dA*dB <= 6, the approximation is exact if def.ppt = 'doherty' or [1].
+
     dims = def.dims;
     dA = dims(1);
     dB = dims(2);
--- RobustnessConeC.m
+++ RobustnessConeC.m
@@ -1,5 +1,5 @@
 function set = RobustnessConeC(def)
-% RobustnessConeC Lower bound on the random robustness entanglement measure cone
+% RobustnessConeC Outer approximation of the random robustness entanglement measure cone
 %
 % {nu rho} = RobustnessConeC([dA dB]) returns the cone such that
 %
@@ -10,6 +10,8 @@ function set = RobustnessConeC(def)
 %
 % The robustness is computed with respect to an approximation of
 % the separable cone given in the parameter 'def', obtained by SymmetricExtensionDef.
+%
+% For dA*dB <= 6, the approximation is exact if def.ppt = 'doherty' or [1].
     dims = def.dims;
     dA = dims(1);
     dB = dims(2);
