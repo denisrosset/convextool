105ec4def9a7995c9d3a70d8afef2a553db19d2d
Denis Rosset <physics@denisrosset.com>
2017-02-28 19:47:52 +0800

    Optimized path is not worth the complexity for now.

--- MultiIndex.m
+++ MultiIndex.m
@@ -2,24 +2,14 @@ classdef MultiIndex
 % MultiIndex provides efficient implementations of ind2sub/sub2ind
 % vectorizing operations and avoiding the use of cell arrays
 %
-% Setting optimize = 1 will switch the algorithm to 
-% "fast integer by precomputation of a reciprocal"; we lift the algorithm
-% of libdivide.com
-%
-% For fastest results, call the functions with integer types such as 'uint32'
-% or 'uint64'.
+% For fastest results, call the functions with the 'uint32' integer
+% type, with the caveat that unsigned integers will be provided back.
     properties(SetAccess = immutable)
         n;
         dims;
     end
     properties(Access = private)
         cumProd;
-        % constants for fast integer division
-        optimize;
-        pre_shift;
-        multiplier;
-        increment;
-        full_shift;
     end
     methods
         function obj = MultiIndex(dims, optimize)
@@ -53,30 +43,19 @@ classdef MultiIndex
             nRows = length(ind);
             switch obj.n
               case 0
-                sub = zeros(nRows, 0);
+                sub = zeros(nRows, 0, class(ind));
               case 1
                 sub = ind;
               otherwise
-                if ~obj.optimize
-                    ind = uint32(ind - 1); % convert to 0-based indices
-                    sub = zeros(nRows, obj.n, 'uint32');
-                    for i = 1:obj.n
-                        % TODO: replace modulo by libdivide-like method
-                        sub(:, i) = mod(ind, uint32(obj.dims(i)));
-                        ind = ind - sub(:, i);
-                        ind = ind ./ obj.dims(i);
-                    end
-                    sub = cast(sub + 1, class(ind)); % convert back to 1-based indices
-                else
-                    sub = MultiIndex.indToSubOptimized(uint32(ind), ...
-                                                       uint32(obj.dims), ...
-                                                       obj.pre_shift, ...
-                                                       obj.multiplier, ...
-                                                       obj.increment, ...
-                                                       obj.full_shift);
+                ind = uint32(ind - 1); % convert to 0-based indices
+                sub = zeros(nRows, obj.n, 'uint32');
+                for i = 1:obj.n
+                    sub(:, i) = mod(ind, uint32(obj.dims(i)));
+                    ind = ind - sub(:, i);
+                    ind = ind ./ obj.dims(i);
                 end
+                sub = cast(sub + 1, class(ind)); % convert back to 1-based indices
             end
-            sub = cast(sub, class(ind));
         end
         function ind = subToInd(obj, sub)
         % given a (m x n) matrix of subindices, returns the vector of the m corresponding indices
@@ -98,27 +77,6 @@ classdef MultiIndex
                 else
                     ind = (double(obj.cumProd) * (sub' - 1) + 1)';
                 end
-            end
-        end
-    end
-    methods(Static)
-        function sub = indToSubOptimized(ind, dims, pre_shift, multiplier, increment, full_shift)
-            ind = ind - 1; % convert to 0-based indices
-            n = length(dims);
-            sub = zeros(length(ind), n, 'uint32');
-            for i = 1:length(pre_shift)
-                % we use the second "emitted code" suggestion from libdivide.c
-                rem = ind;
-                if pre_shift(i) > 0
-                    rem = bitshift(rem, -pre_shift(i));
-                end
-                if increment(i)
-                    rem1 = uint32(bitshift(multiplier(i) * uint64(rem) + multiplier(i), -full_shift(i)));
-                else
-                    rem1 = uint32(bitshift(multiplier(i) * uint64(rem), -full_shift(i)));
-                end
-                sub(:, i) = (ind - dims(i)*rem1) + 1;
-                ind = rem;
             end
         end
     end
