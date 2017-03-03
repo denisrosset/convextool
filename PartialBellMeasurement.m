119b5c245c05dac515cfb10e029562ae1e3edd3e
Denis Rosset <physics@denisrosset.com>
2017-03-03 23:58:31 +0800

    Partial Bell measurement doc.

--- PartialBellMeasurement.m
+++ PartialBellMeasurement.m
@@ -1,5 +1,5 @@
 function BM = PartialBellMeasurement
-% BellMeasurement Returns a 1x4 cell array containing 4x4 Herm. matrices
+% PartialBellMeasurement Returns a 1x2 cell array containing 4x4 Herm. matrices
     BM = cell(1, 2);
     BM{1} = [1 0 0 1 % phi plus
              0 0 0 0
